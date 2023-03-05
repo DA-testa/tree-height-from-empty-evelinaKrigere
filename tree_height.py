4996e9013fb3e1abcc30bc4a4b83fd374b267f38
evelinaKrigere <124894388+evelinaKrigere@users.noreply.github.com>
2023-03-05 15:57:33 +0000

    1_meg

--- tree_height.py
+++ tree_height.py
@@ -8,7 +8,20 @@ import numpy
 def compute_height(n, parents):
     # Write this function
     max_height = 0
+    dzilums1 = [0] * n
     # Your code here
+    for i in range(n):
+        dzilums = 0
+        elements = i
+        while elements != -1:
+            dzilums += 1
+            elements = parents[elements]
+        elements1 = i
+        while elements1 != -1:
+            dzilums1[elements1] = dzilums
+            dzilums -= 1
+            elements1 = parents[elements1]
+    max_height = max(dzilums1)
     return max_height
 
 
@@ -17,10 +30,17 @@ def main():
     
     # let user input file name to use, don't allow file names with letter a
     # account for github input inprecision
-    
+    n = int(input())
     # input number of elements
     # input values in one variable, separate with space, split these values in an array
     # call the function and output it's result
+     input_values = input().split()
+    parents = []
+    for i in range(n):
+        parents.append(int(input_values[i]))
+
+
+    print(compute_height(n, parents))
     pass
 
 # In Python, the default limit on recursion depth is rather low,
